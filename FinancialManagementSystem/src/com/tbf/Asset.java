9460c899891ba76f4682f883331c101254abccd3
BrettBerg316 <54595160+BrettBerg316@users.noreply.github.com>
2020-02-13 14:07:02 -0600

    Changes to Asset.java and DataConverter.java
    Changed the private variables in Asset.java to protected variables, started working on DataConverter

--- FinancialManagementSystem/src/com/tbf/Asset.java
+++ FinancialManagementSystem/src/com/tbf/Asset.java
@@ -1,25 +1,25 @@
 package com.tbf;
 
 public abstract class Asset {
-	private String code;
-	private String id;
-	private String label;
-	
+	protected String code;
+	protected String id;
+	protected String label;
+
 	public Asset(String code, String id, String label) {
 		super();
 		this.code = code;
 		this.id = id;
 		this.label = label;
 	}
-	
+
 	public String getCode() {
 		return code;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getLabel() {
 		return label;
 	}
--- FinancialManagementSystem/src/com/tbf/DataConverter.java
+++ FinancialManagementSystem/src/com/tbf/DataConverter.java
@@ -1,8 +1,34 @@
 package com.tbf;
+import java.io.File;
+import java.util.Scanner;
 
 public class DataConverter {
-	public static void main(String[] args) {
-     
+	public static List<Asset> parseDataFile(){
+		List<Asset> result = new ArrayList<Asset>();
+		File f = new File("data/Assets.dat");
+		Scanner s;
+		try {
+			s = new Scanner(f);
+		} catch(FileNotFoundException fnfe) {
+			throw new RuntimeException(fnfe);
+		}
+		int number = s.hasNext();
+		for(int i=0; i < number; i++) {
+			String line = s.nextLine();
+			if(!line.trim().isEmpty()) {
+				Asset a = null;
+				String tokens[] = line.split(";");
+				String code = tokens[0];
+				String id = tokens[1];
+				String label = tokens[2];
+				if(tokens[1].equals("D")) {
+					a = new Asset();
+				}
+
+			}
+		}
+
+
     }
 
 }
