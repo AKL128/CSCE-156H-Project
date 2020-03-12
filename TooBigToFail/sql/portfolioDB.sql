f91970ba4e49792589d3c24a971f56745002ac7c
AKL128 <54521114+AKL128@users.noreply.github.com>
2020-03-12 16:54:46 -0500

    Add files via upload

--- TooBigToFail/sql/portfolioDB.sql
+++ TooBigToFail/sql/portfolioDB.sql
@@ -31,7 +31,9 @@ create table if not exists Address (
   city varchar(255),
   stateId int,
   zipCode varchar(255),
-  countryId int
+  countryId int,
+  foreign key (stateId) references State(stateId),
+  foreign key (countryId) references Country(countryId)
 )engine=InnoDB,collate=latin1_general_cs;
 
 create table if not exists Person (
@@ -41,9 +43,16 @@ create table if not exists Person (
   firstName varchar(255),
   lastName varchar(255),
   addressId int not null,
-  emailId varchar(255)
+  foreign key (addressId) references Address(addressId)
 )engine=InnoDB,collate=latin1_general_cs;
 
+create table if not exists PersonEmail (
+	personEmailId int not null primary key auto_increment,
+    personId int,
+    emailId int,
+    foreign key (personId) references Person(personId),
+    foreign key (emailId) references Email(emailId)
+    )engine=InnoDB,collate=latin1_general_cs;
 
 create table if not exists Asset(
   assetId int not null primary key auto_increment,
@@ -81,112 +90,132 @@ create table if not exists PortfolioAsset (
   foreign key (assetId) references Asset(assetId)
 )engine=InnoDB,collate=latin1_general_cs;
 
---- Persons
-insert into Person (personId, personCode, brokerData, firstName, lastName, addressId, emailId) values 
-(1, 'VB76HV', 'E,sec725', 'Amara', 'Deeson', 101, 'adeeson0@4shared.com'),
-(2, 'CWQ9R0', 'J,sec368', 'Fred', 'Biffin', 102, 'fbiffin1@shutterfly.com'),
-(3, 'BASMCU', null, 'Sergei', 'Rignoldes', 103, 'srignoldes2@hhs.gov'),
-(4, 'B034B9', null, 'Tamiko', 'Ropartz', 104, 'tropartz3@ox.ac.uk'),
-(5, 'K26CR3', 'E,sec564', 'Opaline', 'Lacaze', 105, 'olacaze4@blogtalkradio.com'),
-(6, 'NA2V3T', null, 'Dag', 'McAllen', 106, 'dmcallen5@dagondesign.com'),
-(7, 'VIO4C2', 'J,sec742', 'Glad', 'Applegate', 107, 'gapplegate6@si.edu'),
-(8, 'R3M84Y', null, 'Simona', 'Brant', 108, 'sbrant7@mayoclinic.com'),
-(9, 'GBRFGX', null, 'Barton', 'Mansour', 109, 'bmansour8@e-recht24.de'),
-(10, '8OMYLX', 'J,sec116', 'Lucky', 'Lowbridge', 110, 'llowbridge9@trellian.com'),
-(11, 'MVLJFF', 'E,sec581', 'Alano', 'Grevel', 111, 'agrevela@earthlink.net'),
-(12, 'Y0QRRR', null, 'Emera', 'Schirach', 112, 'eschirachb@abc.net.au'),
-(13, 'SI6KNE', 'J,sec732', 'Minna', 'Seabrooke', 113, 'mseabrookec@live.com'),
-(14, 'JSTWZL', 'J,sec824', 'Marco', 'Gasson', 114, 'mgassond@cmu.edu'),
-(15, 'AI4H73', 'J,sec324', 'Ikey', 'Shelborne', 115, 'ishelbornee@digg.com'),
-(16, '8H1DJB', 'J,sec020', 'Brand', 'Calcut', 116, 'bcalcutf@shutterfly.com');
-
---- Addresses
-insert into Address (addressId, street, city, stateId, zipCode, countryId) values 
-	(101, '24624 Glacier Hill Alley', 'Springfield', 201, '22156', 301),
-    (102, '04 Talmadge Crossing', 'Hampton', 201, '23663', 301),
-    (103, '0 Autumn Leaf Plaza', 'Los Angeles', 202, '90045', 301),
-    (104, '796 Grover Crossing', 'Richmond', 201, '23208', 301),
-    (105, '18169 Knutson Point', 'Arlington', 203, '76096', 301),
-    (106, '00 Esker Road', 'Athens', 204, '30605', 301),
-    (107, '61 Weeping Birch Parkway', 'Youngstown', 205, '44511', 301),
-    (108, '57 Holy Cross Circle', 'Des Moines', 206, '50369', 301),
-    (109, '63417 Sutteridge Junction', 'Bakersfield', 202, '93305', 301),
-	(110, '6574 Forest Trail', 'Phoenix', 207, '85015', 301),
-    (111, '44 Graceland Terrace', 'Lincoln', 208, '68524', 301),
-    (112, '3 Columbus Center', 'Jamaica', 209, '11499', 301),
-    (113, '64202 Mcbride Road', 'Little Rock', 210, '72204', 301),
-    (114, '09 Cascade Plaza', 'Visalia', 202, '93291', 301),
-    (115, '5 Oakridge Pass', 'Yakima', 211, '98907', 301),
-    (116, '9 Colorado Alley', 'Phoenix', 207, '85015', 301);
+--- Countries
+insert into Country (countryName) values ('United States');
 
 --- States
-insert into State (stateId, stateName) values 
-	(201, 'Virginia'),
-    (202, 'California'),
-    (203, 'Texas'),
-    (204, 'Georgia'),
-    (205, 'Ohio'),
-    (206, 'Iowa'),
-    (207, 'Arizona'),
-    (208, 'Nebraska'),
-    (209, 'New York'),
-    (210, 'Arkansas'),
-    (211, 'Washington');
+insert into State (stateName) values 
+	('Virginia'),
+    ('California'),
+    ('Texas'),
+    ('Georgia'),
+    ('Ohio'),
+    ('Iowa'),
+    ('Arizona'),
+    ('Nebraska'),
+    ('New York'),
+    ('Arkansas'),
+    ('Washington');
 
---- Countries
-insert into Country (countryId, countryName) values (301, 'United States');
+--- Addresses
+insert into Address (street, city, stateId, zipCode, countryId) values 
+	('24624 Glacier Hill Alley', 'Springfield', (SELECT stateId FROM State WHERE stateName = "Virginia"), '22156', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('04 Talmadge Crossing', 'Hampton', (SELECT stateId FROM State WHERE stateName = "Virginia"), '23663', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('0 Autumn Leaf Plaza', 'Los Angeles', (SELECT stateId FROM State WHERE stateName = "California"), '90045', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('796 Grover Crossing', 'Richmond', (SELECT stateId FROM State WHERE stateName = "Virginia"), '23208', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('18169 Knutson Point', 'Arlington', (SELECT stateId FROM State WHERE stateName = "Texas"), '76096', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('00 Esker Road', 'Athens', (SELECT stateId FROM State WHERE stateName = "Georgia"), '30605', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('61 Weeping Birch Parkway', 'Youngstown', (SELECT stateId FROM State WHERE stateName = "Ohio"), '44511', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('57 Holy Cross Circle', 'Des Moines', (SELECT stateId FROM State WHERE stateName = "Iowa"), '50369', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('63417 Sutteridge Junction', 'Bakersfield', (SELECT stateId FROM State WHERE stateName = "Arizona"), '93305', (SELECT countryId FROM Country WHERE countryName = "United States")),
+	('6574 Forest Trail', 'Phoenix', (SELECT stateId FROM State WHERE stateName = "New York"), '85015', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('44 Graceland Terrace', 'Lincoln', (SELECT stateId FROM State WHERE stateName = "Nebraska"), '68524', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('3 Columbus Center', 'Jamaica', (SELECT stateId FROM State WHERE stateName = "Washington"), '11499', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('64202 Mcbride Road', 'Little Rock', (SELECT stateId FROM State WHERE stateName = "Arkansas"), '72204', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('09 Cascade Plaza', 'Visalia', (SELECT stateId FROM State WHERE stateName = "Iowa"), '93291', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('5 Oakridge Pass', 'Yakima', (SELECT stateId FROM State WHERE stateName = "Washington"), '98907', (SELECT countryId FROM Country WHERE countryName = "United States")),
+    ('9 Colorado Alley', 'Phoenix', (SELECT stateId FROM State WHERE stateName = "Nebraska"), '85015', (SELECT countryId FROM Country WHERE countryName = "United States"));
+    
+--- Emails
+insert into Email (emailName) values 
+	('adeeson0@4shared.com'),
+    ('fbiffin1@shutterfly.com'),
+    ('srignoldes2@hhs.gov'),
+    ('tropartz3@ox.ac.uk'),
+    ('olacaze4@blogtalkradio.com'),
+    ('dmcallen5@dagondesign.com'),
+    ('gapplegate6@si.edu'),
+    ('sbrant7@mayoclinic.com'),
+    ('bmansour8@e-recht24.de'),
+    ('llowbridge9@trellian.com'),
+    ('agrevela@earthlink.net'),
+    ('eschirachb@abc.net.au'),
+    ('mseabrookec@live.com'),
+    ('mgassond@cmu.edu'),
+    ('ishelbornee@digg.com'),
+    ('bcalcutf@shutterfly.com');
+    
+--- Persons
+insert into Person (personCode, brokerData, firstName, lastName, addressId) values 
+	('VB76HV', 'E,sec725', 'Amara', 'Deeson', (SELECT addressId FROM Address WHERE street = '24624 Glacier Hill Alley')),
+	('CWQ9R0', 'J,sec368', 'Fred', 'Biffin', (SELECT addressId FROM Address WHERE street = '04 Talmadge Crossing')),
+	('BASMCU', null, 'Sergei', 'Rignoldes', (SELECT addressId FROM Address WHERE street = '0 Autumn Leaf Plaza')),
+	('B034B9', null, 'Tamiko', 'Ropartz', (SELECT addressId FROM Address WHERE street = '796 Grover Crossing')),
+	('K26CR3', 'E,sec564', 'Opaline', 'Lacaze', (SELECT addressId FROM Address WHERE street = '18169 Knutson Point')),
+	('NA2V3T', null, 'Dag', 'McAllen', (SELECT addressId FROM Address WHERE street = '00 Esker Road')),
+	('VIO4C2', 'J,sec742', 'Glad', 'Applegate', (SELECT addressId FROM Address WHERE street = '61 Weeping Birch Parkway')),
+	('R3M84Y', null, 'Simona', 'Brant', (SELECT addressId FROM Address WHERE street = '57 Holy Cross Circle')),
+	('GBRFGX', null, 'Barton', 'Mansour', (SELECT addressId FROM Address WHERE street = '63417 Sutteridge Junction')),
+	('8OMYLX', 'J,sec116', 'Lucky', 'Lowbridge', (SELECT addressId FROM Address WHERE street = '6574 Forest Trail')),
+	('MVLJFF', 'E,sec581', 'Alano', 'Grevel', (SELECT addressId FROM Address WHERE street = '44 Graceland Terrace')),
+	('Y0QRRR', null, 'Emera', 'Schirach', (SELECT addressId FROM Address WHERE street = '3 Columbus Center')),
+	('SI6KNE', 'J,sec732', 'Minna', 'Seabrooke', (SELECT addressId FROM Address WHERE street = '64202 Mcbride Road')),
+	('JSTWZL', 'J,sec824', 'Marco', 'Gasson', (SELECT addressId FROM Address WHERE street = '09 Cascade Plaza')),
+	('AI4H73', 'J,sec324', 'Ikey', 'Shelborne', (SELECT addressId FROM Address WHERE street = '5 Oakridge Pass')),
+	('8H1DJB', 'J,sec020', 'Brand', 'Calcut', (SELECT addressId FROM Address WHERE street = '9 Colorado Alley'));
+
 
 --- Assets
 insert into Asset (assetId, assetCode, assetType, assetLabel, apr, balance, quarterlyDividend, baseRateOfReturn, betaMeasure, stockSymbol, sharePrice, baseOmegaMeasure, totalValue) values 
-	(401, 'YMj2jjQt', 'D', 'Photobug', 83.89, null, null, null, null, null, null, null, null),
-	(402, 'FSacHJRA', 'D', 'Minyx', 36.5, null, null, null, null, null, null, null, null),
-	(403, 'TTAVkJ1z', 'D', 'Kimia', 80.46, null, null, null, null, null, null, null, null),
-	(404, 'WSWGFGOt', 'D', 'Tazz', 2.14, null, null, null, null, null, null, null, null),
-	(405, 'Z2XGqhbU', 'D', 'Photobug', 22.17, null, null, null, null, null, null, null, null),
-	(406, 'OYWhU0b8', 'D', 'Agivu', 13.34, null, null, null, null, null, null, null, null),
-	(407, 'NMgkglJq', 'D', 'Gigabox', 75.03, null, null, null, null, null, null, null, null),
-	(408, 'tAceWN9L', 'D', 'Yotz', 32.12, null, null, null, null, null, null, null, null),
-	(409, 'zUcSIYPE', 'D', 'Thoughtbridge', 70.95, null, null, null, null, null, null, null, null),
-	(410, 'giff6C5r', 'D', 'Dablist', 70.77, null, null, null, null, null, null, null, null),
-	(411, '89uKEpsi', 'S', 'Buzzster', null, 1738472.35, 53.02, 0.14452, 43, 'ACV', 99737.57, null, null),
-	(412, '4WDyHL0t', 'S', 'Realbridge', null, 4698672.52, 13.78, 0.85331, 21, 'PRA', 1963.33, null, null),
-	(413, 'OM3XnDC7', 'S', 'Trupe', null, 9816301.0, 30.42, 0.09815, 87, 'RESN', 56916.89, null, null),
-	(414, 's0rhIsHa', 'S', 'Topdrive', null, 6828861.31, 27.51, 0.87092, 77.77, 'KND', 834.36, null, null),
-	(415, 'fWHZRzrb', 'S', 'Aimbo', null, 353829.81, 8.21, 0.1455, 12.21, 'SSFN', 21662.82, null, null),
-	(416, '0KZ7GLay', 'S', 'Twimbo', null, 9852915.0, 82.89, 0.43662, 44.4, 'TIER', 36996.0, null, null),
-	(417, 'CvGUIwRO', 'S', 'Meemm', null, 2656051.28, 36.34, 0.41667, 69, 'EW', 42952.67, null, null),
-	(418, 'Q2pTejug', 'S', 'Skyvu', null, 5381220.0, 38.14, 0.55755, 65.57, 'HABT', 74776.52, null, null),
-	(419, 'un1qqk3u', 'S', 'Talane', null, 3163798.35, 6.21, 0.86828, 1, 'Y', 54341.31, null, null),
-	(420, '4agAKJEq', 'S', 'Rhycero', null, 4224872.0, 64.13, 0.26453, 99.9, 'WTW', 33628.92, null, null),
-	(421, 'ZEnSzfbI', 'P', 'Roombo', null, 3519178.68, 15.08, null, null, null, null, 0.86325, 6514528.34),
-	(422, 'kzMLkD2z', 'P', 'Kwimbee', null, 3125087.79, 38.38, null, null, null, null, 0.0325, 729509.87),
-	(423, 'N9dpHjke', 'P', 'Avamm', null, 8901536.0, 12.37, null, null, null, null, 0.76147, 666338.36),
-	(424, 'N4ujtpKT', 'P', 'Fadeo', null, 5659081.7, 82.82, null, null, null, null, 0.71471, 549900.29),
-	(425, 'EbTjfJsg', 'P', 'JumpXS', null, 325017.3, 94.36, null, null, null, null, 0.70894, 5780667.82),
-	(426, 'hm7FbEF2', 'P', 'Livetube', null, 8482339.0, 11.79, null, null, null, null, 0.36347, 8560968.0),
-	(427, 'zd68EsoK', 'P', 'Vidoo', null, 3567412.0, 63.69, null, null, null, null, 0.06971, 2774868.86),
-	(428, 'D6j50RPV', 'P', 'Zoozzy', null, 9500256.0, 21.91, null, null, null, null, 0.62879, 8101357.0),
-	(429, 'bC51jaAV', 'P', 'Tagchat', null, 5180527.3, 11.15, null, null, null, null, 0.35742, 7391383.09),
-	(430, 'LS30qQV5', 'P', 'Wordify', null, 8041050.18, 34.4, null, null, null, null, 0.03922, 9126317.0);
+	('YMj2jjQt', 'D', 'Photobug', 83.89, null, null, null, null, null, null, null, null),
+	('FSacHJRA', 'D', 'Minyx', 36.5, null, null, null, null, null, null, null, null),
+	('TTAVkJ1z', 'D', 'Kimia', 80.46, null, null, null, null, null, null, null, null),
+	('WSWGFGOt', 'D', 'Tazz', 2.14, null, null, null, null, null, null, null, null),
+	('Z2XGqhbU', 'D', 'Photobug', 22.17, null, null, null, null, null, null, null, null),
+	('OYWhU0b8', 'D', 'Agivu', 13.34, null, null, null, null, null, null, null, null),
+	('NMgkglJq', 'D', 'Gigabox', 75.03, null, null, null, null, null, null, null, null),
+	('tAceWN9L', 'D', 'Yotz', 32.12, null, null, null, null, null, null, null, null),
+	('zUcSIYPE', 'D', 'Thoughtbridge', 70.95, null, null, null, null, null, null, null, null),
+	('giff6C5r', 'D', 'Dablist', 70.77, null, null, null, null, null, null, null, null),
+	('89uKEpsi', 'S', 'Buzzster', null, 1738472.35, 53.02, 0.14452, 43, 'ACV', 99737.57, null, null),
+	('4WDyHL0t', 'S', 'Realbridge', null, 4698672.52, 13.78, 0.85331, 21, 'PRA', 1963.33, null, null),
+	('OM3XnDC7', 'S', 'Trupe', null, 9816301.0, 30.42, 0.09815, 87, 'RESN', 56916.89, null, null),
+	('s0rhIsHa', 'S', 'Topdrive', null, 6828861.31, 27.51, 0.87092, 77.77, 'KND', 834.36, null, null),
+	('fWHZRzrb', 'S', 'Aimbo', null, 353829.81, 8.21, 0.1455, 12.21, 'SSFN', 21662.82, null, null),
+	('0KZ7GLay', 'S', 'Twimbo', null, 9852915.0, 82.89, 0.43662, 44.4, 'TIER', 36996.0, null, null),
+	('CvGUIwRO', 'S', 'Meemm', null, 2656051.28, 36.34, 0.41667, 69, 'EW', 42952.67, null, null),
+	('Q2pTejug', 'S', 'Skyvu', null, 5381220.0, 38.14, 0.55755, 65.57, 'HABT', 74776.52, null, null),
+	('un1qqk3u', 'S', 'Talane', null, 3163798.35, 6.21, 0.86828, 1, 'Y', 54341.31, null, null),
+	('4agAKJEq', 'S', 'Rhycero', null, 4224872.0, 64.13, 0.26453, 99.9, 'WTW', 33628.92, null, null),
+	('ZEnSzfbI', 'P', 'Roombo', null, 3519178.68, 15.08, null, null, null, null, 0.86325, 6514528.34),
+	('kzMLkD2z', 'P', 'Kwimbee', null, 3125087.79, 38.38, null, null, null, null, 0.0325, 729509.87),
+	('N9dpHjke', 'P', 'Avamm', null, 8901536.0, 12.37, null, null, null, null, 0.76147, 666338.36),
+	('N4ujtpKT', 'P', 'Fadeo', null, 5659081.7, 82.82, null, null, null, null, 0.71471, 549900.29),
+	('EbTjfJsg', 'P', 'JumpXS', null, 325017.3, 94.36, null, null, null, null, 0.70894, 5780667.82),
+	('hm7FbEF2', 'P', 'Livetube', null, 8482339.0, 11.79, null, null, null, null, 0.36347, 8560968.0),
+	('zd68EsoK', 'P', 'Vidoo', null, 3567412.0, 63.69, null, null, null, null, 0.06971, 2774868.86),
+	('D6j50RPV', 'P', 'Zoozzy', null, 9500256.0, 21.91, null, null, null, null, 0.62879, 8101357.0),
+	('bC51jaAV', 'P', 'Tagchat', null, 5180527.3, 11.15, null, null, null, null, 0.35742, 7391383.09),
+	('LS30qQV5', 'P', 'Wordify', null, 8041050.18, 34.4, null, null, null, null, 0.03922, 9126317.0);
 
 
 --- Portfolios
-insert into Portfolio (portfolioId, portCode, ownerId, managerId, beneficiaryId) values 
-	(501, 'PD111', 1, 2, null),
-    (502, 'PT001', 3, 4, null),
-    (503, 'PF001', 5, 1, null),
-    (504, 'PT002', 6, 7, null),
-    (505, 'PF002', 8, 9, null),
-    (506, 'PD001', 10, 11, null),
-    (507, 'PZ001', 12, 14, null),
-    (508, 'Pz002', 15, 16, null);
+insert into Portfolio (portCode, ownerId, managerId, beneficiaryId) values 
+	('PD111', (SELECT personId FROM Person WHERE lastName = 'Deeson' AND firstName = 'Amara'), (SELECT personId FROM Person WHERE lastName = 'Biffin' AND firstName = 'Fred'), null),
+    ('PT001', (SELECT personId FROM Person WHERE lastName = 'Rignoldes' AND firstName = 'Sergei'), (SELECT personId FROM Person WHERE lastName = 'Ropartz' AND firstName = 'Tamiko'), null),
+    ('PF001', (SELECT personId FROM Person WHERE lastName = 'Lacaze' AND firstName = 'Opaline'), (SELECT personId FROM Person WHERE lastName = 'McAllen' AND firstName = 'Dag'), null),
+    ('PT002', (SELECT personId FROM Person WHERE lastName = 'Applegate' AND firstName = 'Glad'), (SELECT personId FROM Person WHERE lastName = 'Brant' AND firstName = 'Simona'), null),
+    ('PF002', (SELECT personId FROM Person WHERE lastName = 'Mansour' AND firstName = 'Barton'), (SELECT personId FROM Person WHERE lastName = 'Lowbridge' AND firstName = 'Lucky'), null),
+    ('PD001', (SELECT personId FROM Person WHERE lastName = 'Grevel' AND firstName = 'Alano'), (SELECT personId FROM Person WHERE lastName = 'Schirach' AND firstName = 'Emera'), null),
+    ('PZ001', (SELECT personId FROM Person WHERE lastName = 'Seabrooke' AND firstName = 'Minna'), (SELECT personId FROM Person WHERE lastName = 'Gasson' AND firstName = 'Marco'), null),
+    ('PZ002', (SELECT personId FROM Person WHERE lastName = 'Shelborne' AND firstName = 'Ikey'), (SELECT personId FROM Person WHERE lastName = 'Calcut' AND firstName = 'Brand'), null);
 
 --- Portfolio Assets
 insert into PortfolioAsset (portfolioId, assetId, assetAmount) values 
-	(501, 401, 10120.23),
-    (501, 401, 400),
-    (501, 411, 50),
-    (501, 421, 89),
+	((SELECT portfolioId FROM Portfolio WHERE portCode = 'PD111'), (SELECT assetId FROM Asset WHERE assetCode = 'YMj2jjQt'), 10120.23),
+    ((SELECT portfolioId FROM Portfolio WHERE portCode = 'PD111'), (SELECT assetId FROM Asset WHERE assetCode = '89uKEpsi'), 400),
+    ((SELECT portfolioId FROM Portfolio WHERE portCode = 'PD111'), (SELECT assetId FROM Asset WHERE assetCode = 'ZEnSzfbI'), 50),
+    ((SELECT portfolioId FROM Portfolio WHERE portCode = 'PD111'), (SELECT assetId FROM Asset WHERE assetCode = 'FSacHJRA'), 89),
     (502, 402, 54321.01),
     (502, 412, 98),
     (502, 413, 64),
