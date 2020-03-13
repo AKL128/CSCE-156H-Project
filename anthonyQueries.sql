65340d3be478c9ca9fcd3bb23d2a7e38bd535b09
AKL128 <54521114+AKL128@users.noreply.github.com>
2020-03-13 14:29:17 -0500

    Add files via upload

--- anthonyQueries.sql
+++ anthonyQueries.sql
@@ -1,27 +1,29 @@
 
 -- 1.
-SELECT p.personCode, p.brokerData, p.firstName, p.lastName, a.street, a.city, s.stateName, c.countryName FROM Person p
+SELECT p.personCode, p.brokerData, p.firstName, p.lastName, a.street, a.city, s.stateName, c.countryName, e.emailName FROM Person p
 JOIN Address a ON p.addressId = a.addressId
 JOIN State s ON a.stateId = s.stateId
-JOIN Country c ON a.countryId = c.countryId;
+JOIN Country c ON a.countryId = c.countryId
+LEFT JOIN Email e ON p.personId = e.personId;
 
 -- 2.
 SELECT p.firstName, p.lastName, e.emailName FROM Person p
 LEFT JOIN Email e ON p.personId = e.personId
-WHERE p.personId = 1;
-
+WHERE p.personCode = 'CWQ9R0';
 
 -- 3.
-UPDATE Email SET personId = 1 WHERE emailId = 19;
+INSERT INTO Email (personId, emailName) VALUES ((SELECT personId FROM Person WHERE lastName = 'Calcut' AND firstName = 'Brand')
+												, 'AddedEmail@gmail.com');
 
 -- 4
-UPDATE Email SET emailName = "changedEmail@what.com" WHERE emailId = 1;
+UPDATE Email SET emailName = "changedEmail@what.com" 
+WHERE emailId = 500 AND emailName = 'bcalcutf@shutterfly.com';
 
 -- 5
 DELETE FROM Email WHERE personId = 1;
-DELETE FROM PortfolioAsset WHERE portfolioId = 1;
+DELETE FROM PortfolioAsset WHERE portfolioId = (SELECT portfolioId FROM Portfolio WHERE ownerId = 1);
 DELETE FROM Portfolio WHERE ownerId = 1;
-DELETE FROM Person WHERE personId = 1;
+DELETE FROM Person WHERE personCode = 'VB76HV' AND personId = 1;
 
 -- 6
 INSERT INTO Person (personCode, brokerData, firstName, lastName, addressId) values
@@ -38,7 +40,7 @@ SELECT a.* FROM Person p
 LEFT JOIN Portfolio po ON p.personId = po.ownerId
 LEFT JOIN PortfolioAsset pa ON po.portfolioId = pa.portfolioId
 LEFT JOIN Asset a ON pa.assetId = a.assetId
-WHERE p.personId = 3;
+WHERE p.firstName = 'Brand' AND p.lastName = 'Calcut';
 
 -- 9
 INSERT INTO Asset (assetCode, assetType, assetLabel, apr, balance, quarterlyDividend, baseRateOfReturn, betaMeasure, stockSymbol, sharePrice, baseOmegaMeasure, totalValue) values
