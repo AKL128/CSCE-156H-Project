a6b8042858201db5c9ae885dc67ac418a32b27a8
AKL128 <anthonyluu128@gmail.com>
2020-04-03 06:09:57 -0500

    fixed the main problem

--- TooBigToFail/src/com/tbf/PortfolioReport.java
+++ TooBigToFail/src/com/tbf/PortfolioReport.java
@@ -36,22 +36,6 @@ public class PortfolioReport {
 		List<Asset> aList = Asset.loadAllAssets();
 		List<Person> pList = Person.loadAllPersons();
 		
-//		List<Portfolio> porter = Portfolio.loadAllPortfolios();
-//		List<Address> add = Address.loadAllAddresses();
-//		List<Person> personList = Person.loadAllPersons();
-//		List<Asset> assetList = Asset.loadAllAssets();
-//		
-//		System.out.println(personList);
-//		for(Person p : personList) {
-//			System.out.println(p.getBrokerData());
-//		}
-		
-//		for(Asset a : assetList) {
-//			System.out.println(a.getRisk());
-//		}
-		
-		
-//		
 		for(Portfolio port : portList) {
 			System.out.println(port);
 		}
@@ -92,84 +76,86 @@ public class PortfolioReport {
 		}
 		
 		System.out.println(summaryReport2);
-			//
-//							String portCode = port.getPortCode();
-			//
-//							StringBuilder detailedReport = new StringBuilder();
-			//
-//							detailedReport.append(String.format("Portfolio %s\n", portCode));
-			//
-//							String ownerString = "";
-//							String managerString = "";
-//							String beneficiaryString = "";
-			//
-//							Person owner = port.getOwner();
-//							Person manager = port.getManager();
-//							Person beneficiary = port.getBeneficiary();
-			//
-//							ownerString = port.getOwner().getPersonCode();
-//							managerString = port.getManager().getPersonCode();
-//							if(port.getBeneficiary() != null) {
-//								beneficiaryString = port.getBeneficiary().getPersonCode();
-//							}
-			//
-			//
-//							for(Person p : pList) {
-//								String personCode = p.getPersonCode();
-//								if(personCode.equals(ownerString)) {
-//									detailedReport.append(String.format("Owner:\n%s", p));
-//								}
-//							}
-//							for(Person p : pList) {
-//								String personCode = p.getPersonCode();
-//								if(personCode.equals(managerString)) {
-//									detailedReport.append(String.format("Manager:\n%s", p));
-//								}
-//							}
-//							for(Person p : pList) {
-//								String personCode = p.getPersonCode();
-//								if(personCode.equals(beneficiaryString)) {
-//									detailedReport.append(String.format("Beneficiary:\n%s", p));
-//								}
-//							}
-			//
-//							ArrayList<Asset> assetData = port.getAssetList();
-			//
-//							detailedReport.append(String.format("Assets\n"));
-//							detailedReport.append(String.format("%-8s %-20s %16s %16s %16s %16s\n",
-//									"Code", "Asset", "Return Rate", "Risk", "Annual Return", "Value"));
-			//
-//							double aggregateRisk = 0;
-//							double annualReturnTotal = 0;
-//							ArrayList<Double> riskAtI = new ArrayList<Double>();
-//							double totalValue = 0;
-			//
-//							for(Asset a : assetData) {
-//								double apy = 0.0;
-//								double value = 0.0;
-//								value = a.getValue();
-//								apy = a.getAnnualReturn();
-			//
-//								riskAtI.add((a.getRisk() * a.getValue()));
-//								annualReturnTotal += a.getAnnualReturn();
-//								totalValue += a.getValue();
-			//
-//								detailedReport.append(String.format("%-8s %-20s %16.2f%% %16.2f %16.2f %16.2f\n",
-//									a.getCode(), a.getLabel(), a.getReturnRate(), a.getRisk(), a.getAnnualReturn(), a.getValue()));
-			//
-//								aggregateRisk = getAggregateRisk(riskAtI, totalValue);
-			//
-//							}
-//							detailedReport.append(String.format("-----------------------------------------------------------------------------------------------\n"));
-//							detailedReport.append(String.format("%64.5f %16.2f % 16.2f\n", aggregateRisk, annualReturnTotal, totalValue));
-//							//summaryReport.append(String.format("", ));
-//							System.out.println(detailedReport);
-//							pwR.append(detailedReport);
-//						}
-//					} catch(FileNotFoundException fnfe) {
-//						throw new RuntimeException(fnfe);
-//					}
+
 
 	}
 
 }
+
+//
+//String portCode = port.getPortCode();
+//
+//StringBuilder detailedReport = new StringBuilder();
+//
+//detailedReport.append(String.format("Portfolio %s\n", portCode));
+//
+//String ownerString = "";
+//String managerString = "";
+//String beneficiaryString = "";
+//
+//Person owner = port.getOwner();
+//Person manager = port.getManager();
+//Person beneficiary = port.getBeneficiary();
+//
+//ownerString = port.getOwner().getPersonCode();
+//managerString = port.getManager().getPersonCode();
+//if(port.getBeneficiary() != null) {
+//	beneficiaryString = port.getBeneficiary().getPersonCode();
+//}
+//
+//
+//for(Person p : pList) {
+//	String personCode = p.getPersonCode();
+//	if(personCode.equals(ownerString)) {
+//		detailedReport.append(String.format("Owner:\n%s", p));
+//	}
+//}
+//for(Person p : pList) {
+//	String personCode = p.getPersonCode();
+//	if(personCode.equals(managerString)) {
+//		detailedReport.append(String.format("Manager:\n%s", p));
+//	}
+//}
+//for(Person p : pList) {
+//	String personCode = p.getPersonCode();
+//	if(personCode.equals(beneficiaryString)) {
+//		detailedReport.append(String.format("Beneficiary:\n%s", p));
+//	}
+//}
+//
+//ArrayList<Asset> assetData = port.getAssetList();
+//
+//detailedReport.append(String.format("Assets\n"));
+//detailedReport.append(String.format("%-8s %-20s %16s %16s %16s %16s\n",
+//		"Code", "Asset", "Return Rate", "Risk", "Annual Return", "Value"));
+//
+//double aggregateRisk = 0;
+//double annualReturnTotal = 0;
+//ArrayList<Double> riskAtI = new ArrayList<Double>();
+//double totalValue = 0;
+//
+//for(Asset a : assetData) {
+//	double apy = 0.0;
+//	double value = 0.0;
+//	value = a.getValue();
+//	apy = a.getAnnualReturn();
+//
+//	riskAtI.add((a.getRisk() * a.getValue()));
+//	annualReturnTotal += a.getAnnualReturn();
+//	totalValue += a.getValue();
+//
+//	detailedReport.append(String.format("%-8s %-20s %16.2f%% %16.2f %16.2f %16.2f\n",
+//		a.getCode(), a.getLabel(), a.getReturnRate(), a.getRisk(), a.getAnnualReturn(), a.getValue()));
+//
+//	aggregateRisk = getAggregateRisk(riskAtI, totalValue);
+//
+//}
+//detailedReport.append(String.format("-----------------------------------------------------------------------------------------------\n"));
+//detailedReport.append(String.format("%64.5f %16.2f % 16.2f\n", aggregateRisk, annualReturnTotal, totalValue));
+////summaryReport.append(String.format("", ));
+//System.out.println(detailedReport);
+//pwR.append(detailedReport);
+//}
+//} catch(FileNotFoundException fnfe) {
+//throw new RuntimeException(fnfe);
+//}
